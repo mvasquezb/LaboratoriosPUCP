8e849cfd6b14425c85c2b25aaefcd149e5d36a0d
akawa <serdelrio94@gmail.com>
2017-05-08 21:04:07 -0500

    delrioBranch

--- laboratorios/internal/models.py
+++ laboratorios/internal/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Essay(models.Model):
+    title_text = models.CharField(max_length=200);
+    detail_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title_text
+
+class Test(models.Model):
+    essay = models.ManyToManyRel(Essay)
+    title_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    #1..M with Parameters
+    def __str__(self):
+        return self.title_text
+
+class LbTestParameter(models.Model):
+    #PK
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    #END PK
+    title_text = models.CharField(max_length=200)
+    detail_text = models.CharField(max_length=200)
+    unit_text = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.parameter_title
+
