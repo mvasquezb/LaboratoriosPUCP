7c4686e685f518031e618865c8ab270abf7baff9
Martín Vásquez <pedromvasquezb@gmail.com>
2017-05-10 17:00:13 -0500

    Add base models

--- laboratorios/internal/models.py
+++ laboratorios/internal/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Client(models.Model):
+	name = models.CharField(max_length=1000)
+
+
+class Sale(models.Model):
+	ammount = models.FloatField()
+
+
+class Product(models.Model):
+	description = models.CharField(max_length=100)
+	unit_cost = models.FloatField()
+
+	
