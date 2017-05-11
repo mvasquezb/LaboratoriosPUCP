7d2ada54a9ef4aaaa0389d13b1b5987a84500634
Martín Vásquez <pedromvasquezb@gmail.com>
2017-05-11 15:50:03 -0500

    Add admin auto register

--- laboratorios/laboratorios/settings.py
+++ laboratorios/laboratorios/settings.py
@@ -74,9 +74,7 @@ TEMPLATES = [
         },
     },
     {
-
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
