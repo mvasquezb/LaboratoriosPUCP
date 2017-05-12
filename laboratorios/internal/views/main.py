6295ab252d23135f7f99edceed4ce17a54076e33
Yacs13 <yacs1396@gmail.com>
2017-05-12 18:12:25 -0500

    Hecho por Sameer y Sergio Cama

--- laboratorios/internal/views/main.py
+++ laboratorios/internal/views/main.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from ..models import Sale
@@ -15,6 +16,17 @@ def index(request,
     return render(request, template, context)
 
 
+def editar_ventas(request,
+          template='internal/editar_ventas.html',
+          extra_context=None):
+    venta_shida = Sale.objects.get(id = 1)
+    if(venta_shida):
+        venta_shida.ammount = 3.1416;
+        venta_shida.save()
+    context = {'venta_shida': venta_shida}
+    return render(request, template, context)
+
+
 def lista_ventas(request,
           template='internal/lista_ventas.html',
           extra_context=None):
@@ -24,3 +36,11 @@ def lista_ventas(request,
     # if extra_context is not None:
     #    context.update(extra_context)
     return render(request, template, context)
+
+#Suma simple
+#Hecho por Chic Cherry Cola <3
+def sumita(request,
+           number_1,
+           number_2):
+
+    return HttpResponse("La suma de %s y %s es: %d" % (number_1, number_2, int(number_1) + int(number_2)) )
