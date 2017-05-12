6b26d71208d1529fbe2e879cf58538c63be4a2ff
MilagrosRH <a20110405@pucp.pe>
2017-05-12 18:51:40 -0500

    lista de productos

--- laboratorios/internal/models.py
+++ laboratorios/internal/models.py
@@ -4,15 +4,22 @@ from django.db import models
 
 
 class Client(models.Model):
-	name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Sale(models.Model):
-	ammount = models.FloatField()
+    ammount = models.FloatField()
+
+    def __str__(self):
+        return "Monto venta " + str(self.pk) + ": " + str(self.ammount)
 
 
 class Product(models.Model):
-	description = models.CharField(max_length=100)
-	unit_cost = models.FloatField()
+    description = models.CharField(max_length=100)
+    unit_cost = models.FloatField()
 
-	
+    def __str__(self):
+        return "Descripcion: " + str(self.description) + " Precio: " + str(self.unit_cost)
