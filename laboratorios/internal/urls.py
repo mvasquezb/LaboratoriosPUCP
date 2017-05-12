25e1dea8f0de0573591dd60890957e0346fff532
Yacs13 <yacs1396@gmail.com>
2017-05-12 18:12:25 -0500

    Hecho por Sameer y Sergio Cama

--- laboratorios/internal/urls.py
+++ laboratorios/internal/urls.py
@@ -11,4 +11,10 @@ urlpatterns = [
     url('^list',
         views.main.lista_ventas,
         name='lista_ventas'),
+    url('^edit',
+        views.main.editar_ventas,
+        name='editar_ventas'),
+    url(r'^suma/(?P<number_1>[0-9]+)/(?P<number_2>[0-9]+)',
+        views.main.sumita,
+        name='sumita'),
 ]
