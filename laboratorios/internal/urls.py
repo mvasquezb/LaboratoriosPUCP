37e2fa2ca023326e94ce4d69892a21469bbc9307
Marco Rodriguez <a20111265@pucp.pe>
2017-05-12 16:31:58 -0500

    edit client working and refactor templates folder

--- laboratorios/internal/urls.py
+++ laboratorios/internal/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url('^$', views.main.index, name='index'),
     url('^clients/$', views.clients.index, name='client.index'),
     url('^clients/create$', views.clients.create, name='client.create'),
+    url('^clients/(?P<client_id>[0-9]+)/edit$', views.clients.edit, name='client.edit'),
+    url('^clients/(?P<client_id>[0-9]+)/update$', views.clients.update, name='client.update'),
 ]
