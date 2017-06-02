08848ce84854bf80874d9bfc71d0d3e3089386fe
Marco Rodriguez <a20111265@pucp.pe>
2017-06-02 18:12:48 -0500

    Fix conflict

--- laboratorios/internal/views/__init__.py
+++ laboratorios/internal/views/__init__.py
@@ -4,15 +4,12 @@ from .role import *
 from .user import *
 from .servicerequest import *
 from .test import *
-<<<<<<< HEAD
 from .access import *
 from .essayType import *
 from .testType import *
 from .forms import *
 from .client import *
 from .essayTemplate import *
-=======
 from .service import *
 from .essayfill import *
 from .testfill import *
->>>>>>> aad7e6e8e7b9c6599fb41a11cac665d8eb0a82b8
