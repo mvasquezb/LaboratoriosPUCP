e4ba4c0ba7411d95a5cb030c8ef901e45235f984
faustoinfausto <scama@pucp.pe>
2017-06-05 00:26:32 -0500

    It worked

--- laboratorios/internal/models.py
+++ laboratorios/internal/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+### Clase de las solicitudes
+class RequestStorage(models.Model):
+    numEnsayo = models.IntegerField()
+    muestra = models.CharField(max_length=100)
+    tipoMuestra = models.CharField(max_length=100)
+    cliente = models.CharField(max_length=100)
+    pendiente = models.BooleanField()
+    aprobado = models.BooleanField()
--- laboratorios/internal/urls.py
+++ laboratorios/internal/urls.py
@@ -8,4 +8,15 @@ urlpatterns = [
     url('^$',
         views.main.index,
         name='index'),
+
+    #URL de requestStorage
+    url('^requestStorage/?$',
+        views.requestStorage.index,
+        name='requestStorage.index'),
+    url('^requestStorage/aprobar/?$',
+        views.requestStorage.aprobar,
+        name='requestStorage.aprobar'),
+    url('^requestStorage/rechazar/?$',
+        views.requestStorage.rechazar,
+        name='requestStorage.rechazar'),
 ]
