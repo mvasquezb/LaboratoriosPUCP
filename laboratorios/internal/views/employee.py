5fd7ddee21fd72af1bc83cf69849d38e9fb8c696
Martín Vásquez <pedromvasquezb@gmail.com>
2017-06-05 06:26:30 -0500

    Rename 'employee.detail' view to 'employee.show' Fix employee laboratories and roles not showing

--- laboratorios/internal/views/employee.py
+++ laboratorios/internal/views/employee.py
@@ -10,12 +10,18 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from internal.models import *
 from internal.views.forms import EmployeeForm
 
-def index(request):
+
+def index(request,
+          template='internal/employee/index.html',
+          extra_context=None):
     search = request.GET.get('search')
-    if search:        
-        employee_list = Employee.objects.filter(username__icontains=search).order_by('username')
+    if search:
+        employee_list = Employee.objects.filter(
+            username__icontains=search
+        ).order_by('username')
     else:
         employee_list = Employee.objects.order_by('username')
+
     paginator = Paginator(employee_list, 3)
     page = request.GET.get('page')
     try:
@@ -25,16 +31,22 @@ def index(request):
     except EmptyPage:
         employees = paginator.page(paginator.num_pages)
 
-    return render(request, 'internal/employee/index.html', {'employees_list': employees})
+    context = {
+        'employees_list': employees
+    }
+    if extra_context is not None:
+        context.update(extra_context)
+    return render(request, template, context)
 
 
-def detail(request, pk,
-           template='internal/employee/detail.html'):
+def show(request,
+         pk,
+         template='internal/employee/show.html'):
     employee = get_object_or_404(Employee, pk=pk)
     context = {
-        'selected_laboratories' : list(employee.laboratories.values_list('id', flat=True)),
-        'selected_roles' : list(employee.roles.values_list('id', flat=True)),
-        'custom_employee': employee
+        'selected_laboratories': employee.laboratories.all(),
+        'selected_roles': employee.roles.all(),
+        'employee': employee
     }
     return render(request, template, context)
 
@@ -42,10 +54,10 @@ def detail(request, pk,
 def create(request,
            template='internal/employee/create.html'):
     form = EmployeeForm(request.POST or None)
-    context={
-        'laboratories' : Laboratory.objects.all(),
-        'roles' : Role.objects.all(),
-        'form' : form
+    context = {
+        'laboratories': Laboratory.objects.all(),
+        'roles': Role.objects.all(),
+        'form': form
     }
     if form.is_valid():
         form.save()
@@ -54,16 +66,16 @@ def create(request,
 
 
 def edit(request, pk,
-           template='internal/employee/edit.html'):
+         template='internal/employee/edit.html'):
     employee = get_object_or_404(Employee, pk=pk)
     form = EmployeeForm(request.POST or None, instance=employee)
-    context={
-        'laboratories' : Laboratory.objects.all(),
-        'selected_laboratories' : list(employee.laboratories.values_list('id', flat=True)),
-        'selected_roles' : list(employee.roles.values_list('id', flat=True)),
-        'roles' : Role.objects.all(),
-        'form' : form,
-        'custom_employee' : employee,
+    context = {
+        'laboratories': Laboratory.objects.all(),
+        'selected_laboratories': employee.laboratories.all(),
+        'selected_roles': employee.roles.all(),
+        'roles': Role.objects.all(),
+        'form': form,
+        'custom_employee': employee,
     }
     if form.is_valid():
         form.save()
