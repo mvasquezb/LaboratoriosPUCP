ac62e14ee653c8c97b43e60a98c015df2dc4a01e
Jose <jchudb93@gmail.com>
2017-06-07 19:03:01 -0500

    role crud

--- laboratorios/internal/views/role.py
+++ laboratorios/internal/views/role.py
@@ -9,11 +9,11 @@ from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from internal.models import *
 from internal.views.forms import EmployeeForm
-
+from internal.views.forms import RoleForm
 def index(request, template='internal/role/index.html',extra_context=None):
     search=request.GET.get('search')
     if search:
-        role_list = Role.objects.filter(name_icontains=search).order_by('name')
+        role_list = Role.objects.filter(name__icontains=search).order_by('name')
     else:
         role_list = Role.objects.order_by('name')
 
@@ -26,7 +26,7 @@ def index(request, template='internal/role/index.html',extra_context=None):
     except EmptyPage:
         roles=paginator.page(paginator.num_pages)
     context = {
-        'roles_list': roles,
+        'role_list': roles,
         'paginator':paginator
     }
     if extra_context is not None:
@@ -41,7 +41,7 @@ def show(request, pk, template='internal/role/show.html'):
     }
     return render(request, template, context)
 
-def create(request, pk,
+def create(request,
            template='internal/role/create.html'):
     form = RoleForm(request.POST or None)
     context = {
@@ -49,20 +49,21 @@ def create(request, pk,
         'form': form
     }
     if request.method == 'POST':
-        if form.is_vali():
+        if form.is_valid():
             form.save()
             messages.success(request, 'Se ha creado el rol correctamente')
             return redirect('internal:role.index')
-    return render(request,tmeplate,context)
+    return render(request,template,context)
 
 def edit(request, pk,
          template='internal/role/edit.html'):
     role=get_object_or_404(Role,pk=pk)
-    form = Roleform(request.POST or None,instance=role)
+    form = RoleForm(request.POST or None,instance=role)
     context = {
         'permissions': Permission.objects.all(),
         'selected_permissions': role.permissions.all(),
         'form':form,
+        'custom_role':role,
     }
     if request.method == 'POST':
         if form.is_valid():
@@ -70,7 +71,7 @@ def edit(request, pk,
             messages.success(request,'se ha editadod con exito')
             return redirect('internal:role.index')
         else:
-            messages.warning(request,'por facor corregir')
+            messages.warning(request,'por favor corregir')
     return render(request,template,context)
 
 def delete(request, pk):
