71a498f080313848ef29054ec8dc8cf6625481dd
akawa <serdelrio94@gmail.com>
2017-06-09 18:05:06 -0500

    Added basic workload view

--- laboratorios/internal/urls.py
+++ laboratorios/internal/urls.py
@@ -108,4 +108,7 @@ urlpatterns = [
     url('^servicerequest/(?P<request_id>\d+)/assign_employee/(?P<sample_id>\d+)/?$',
         views.servicerequest.assign_employee,
         name='servicerequest.assign_employee'),
+    url('^servicerequest/view_workload_per_request$',
+        views.servicerequest.workload_view_per_request,
+        name='servicerequest.workload_view_per_request')
 ]
