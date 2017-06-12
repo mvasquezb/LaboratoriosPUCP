aa8240e51a06483ff311790e008b0cc86730617a
MilagrosRH <a20110405@pucp.pe>
2017-06-12 01:39:56 -0500

    vista laboratorio index corregida

--- laboratorios/internal/views/laboratory.py
+++ laboratorios/internal/views/laboratory.py
@@ -16,25 +16,24 @@ from .forms import LaboratoryForm
 def index(request,
           template='internal/laboratory/index.html',
           extra_context=None):
-    search = request.GET.get('search')
-    if search:
-        laboratory_list = Laboratory.objects.filter(
-            name__icontains=search).order_by ('name')
-    else:
-        laboratory_list = Laboratory.objects.order_by ('name')
+    # search = request.GET.get('search')
+    # if search:
+    #     laboratory_list = Laboratory.objects.filter(
+    #         name__icontains=search).order_by ('name')
+    # else:
+    #     laboratory_list = Laboratory.objects.order_by ('name')
 
-    paginator = Paginator(laboratory_list, 5)
-    page = request.GET.get('page')
-    try:
-        laboratorys = paginator.page (page)
-    except PageNotAnInteger:
-        laboratorys = paginator.page(1)
-    except EmptyPage:
-        laboratorys = paginator.page(paginator.num_pages)
-
+    # paginator = Paginator(laboratory_list, 5)
+    # page = request.GET.get('page')
+    # try:
+    #     laboratorys = paginator.page (page)
+    # except PageNotAnInteger:
+    #     laboratorys = paginator.page(1)
+    # except EmptyPage:
+    #     laboratorys = paginator.page(paginator.num_pages)
+    laboratorys = Laboratory.objects.all()
     context = {
-        'laboratorys_list': laboratorys,
-        'paginator': paginator,
+        'laboratorys_list': laboratorys
     }
     if extra_context is not None:
         context.update(extra_context)
