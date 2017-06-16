53bab6590041af19ec837581b83ee544abf6b8b4
Diego Monge <dmonge@pucp.pe>
2017-06-16 15:49:54 -0500

    minor fixes to make servicerequestpriority work

--- laboratorios/internal/models.py
+++ laboratorios/internal/models.py
@@ -401,7 +401,6 @@ class ServiceRequest(SafeDeleteModel):
     external_provider = models.ForeignKey('ExternalProvider', null=True, blank=True)
     observations = models.CharField(max_length=500, null=True, blank=True)
     expected_duration = models.IntegerField(default=10)
-    priority = models.ForeignKey('ServiceRequestPriority', null=True, blank=True)
     registered_date = models.DateTimeField(
         auto_now_add=True,
         auto_now=False,
@@ -411,13 +410,6 @@ class ServiceRequest(SafeDeleteModel):
     def __str__(self):
         return str(self.client) + ' | ' + str(self.state)
 
-@auditlog.register()
-class ServiceRequestPriority(SafeDeleteModel):
-    slug = models.CharField(max_length=20)
-    value = models.PositiveIntegerField()
-
-    def __str__(self):
-        return str(self.slug)
 
 @auditlog.register()
 class ServiceRequestPriority(SafeDeleteModel):
