66fa0fffdbd626ee0c73f5f4ecc3390e6c563187
Martín Vásquez <pedromvasquezb@gmail.com>
2017-06-16 17:25:33 -0500

    Style changes

--- laboratorios/internal/views/laboratory.py
+++ laboratorios/internal/views/laboratory.py
@@ -35,15 +35,23 @@ def services_index(request,
         dicc = dict(request.POST)
         priorities_pk = dicc['priorities_pk[]']
         services_pk = dicc['services_pk[]']
-        for i in range(0,len(services_pk)):
+        for i in range(0, len(services_pk)):
             if priorities_pk[i] != -1:
-                service = ServiceRequest.objects.get(pk=services_pk[i])
-                service.priority = ServiceRequestPriority.objects.get(pk=priorities_pk[i])
+                service = ServiceRequest.all_objects.get(
+                    deleted__isnull=True,
+                    pk=services_pk[i]
+                )
+                service.priority = ServiceRequestPriority.all_objects.get(
+                    deleted__isnull=True,
+                    pk=priorities_pk[i]
+                )
                 service.save()
-    all_services = ServiceRequest.objects.all()
-    laboratory = Laboratory.objects.get(pk=pk)
+    all_services = ServiceRequest.all_objects.filter(
+        deleted__isnull=True
+    )
+    laboratory = Laboratory.all_objects.get(deleted__isnull=True, pk=pk)
     all_employes = laboratory.employees.all()
-    all_priorities = ServiceRequestPriority.objects.all()
+    all_priorities = ServiceRequestPriority.all_objects.filter(deleted__isnull=True)
     laboratory_services = []
     for service in all_services:
         service_supervisor = service.supervisor
@@ -53,31 +61,34 @@ def services_index(request,
                 break
     context = {'laboratory_services': laboratory_services, 'laboratory': laboratory,
                'priorities': all_priorities}
-    template='internal/laboratory/services_index.html'
+    template = 'internal/laboratory/services_index.html'
     return render(request, template, context)
 
 
 def create(request,
-          template='internal/laboratory/create.html',
-          extra_content=None):
+           template='internal/laboratory/create.html',
+           extra_content=None):
     if request.method == 'POST':
         form = LaboratoryForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Se ha creado un nuevo laboratorio exitosamante!')
+            messages.success(
+                request, 'Se ha creado un nuevo laboratorio exitosamante!')
             return redirect('internal:laboratory.index')
         else:
-            #print(form.errors)
-            for field,errors in form.errors.items():
-                if (field=="name") and list(errors)==['Ya existe Laboratory con este Name.']:
-                    messages.error(request, 'Este nombre de laboratorio ya existe, pruebe otro')
+            # print(form.errors)
+            for field, errors in form.errors.items():
+                if (field == "name") and list(errors) == ['Ya existe Laboratory con este Name.']:
+                    messages.error(
+                        request, 'Este nombre de laboratorio ya existe, pruebe otro')
                     return redirect('internal:laboratory.create')
 
             return HttpResponse(form.errors)
 
     else:
         users = Employee.all_objects.filter(deleted__isnull=True)
-        service_hours = LaboratoryServiceHours.all_objects.filter(deleted__isnull=True)
+        service_hours = LaboratoryServiceHours.all_objects.filter(
+            deleted__isnull=True)
         inventories = Inventory.all_objects.filter(deleted__isnull=True)
         essaymethods = EssayMethod.all_objects.filter(deleted__isnull=True)
         form = LaboratoryForm()
@@ -85,26 +96,37 @@ def create(request,
                    'essaymethods': essaymethods, 'form': form}
         return render(request, template, context)
 
+
 def edit(request,
-        pk):
+         pk):
     if request.method == 'POST':
-        instance = Laboratory.objects.get(pk=pk)
+        instance = Laboratory.all_objects.get(
+            deleted__isnull=True,
+            pk=pk
+        )
         aux_form = LaboratoryForm(request.POST or None, instance=instance)
         if aux_form.is_valid():
             aux_form.save()
-            messages.success(request, 'Se ha editado el laboratorio exitosamante')
+            messages.success(
+                request, 'Se ha editado el laboratorio exitosamante')
             return redirect('internal:laboratory.index')
         else:
-            messages.error(request, 'Ya existe un laboratorio con el mismo nombre, ingrese otro')
-            #return HttpResponse(status=204)
+            messages.error(
+                request, 'Ya existe un laboratorio con el mismo nombre, ingrese otro')
+            # return HttpResponse(status=204)
             return redirect('internal:laboratory.edit', pk=pk)
-    else :
-        laboratory = Laboratory.objects.get(pk=pk)
+    else:
+        laboratory = Laboratory.all_objects.get(
+            deleted_isnull=True,
+            pk=pk
+        )
         #
         all_users = Employee.all_objects.filter(deleted__isnull=True)
         selected_users = laboratory.employees.all()
         #
-        all_service_hours = LaboratoryServiceHours.all_objects.filter(deleted__isnull=True)
+        all_service_hours = LaboratoryServiceHours.all_objects.filter(
+            deleted__isnull=True
+        )
         selected_service_hours = laboratory.service_hours
         #
         all_inventories = Inventory.all_objects.filter(deleted__isnull=True)
@@ -130,21 +152,30 @@ def delete(request, pk):
     messages.success(request, 'Se ha borrado el laboratorio existosamente')
     return redirect('internal:laboratory.index')
 
+
 def show(request,
-        pk):
+         pk):
     if request.method == 'POST':
-        instance = Laboratory.objects.get(pk=pk)
+        instance = Laboratory.all_objects.get(
+            deleted_isnull=True,
+            pk=pk
+        )
         aux_form = LaboratoryForm(request.POST or None, instance=instance)
         if aux_form.is_valid():
             aux_form.save()
             return redirect('internal:laboratory.index')
-    else :
-        laboratory = Laboratory.objects.get(pk=pk)
+    else:
+        laboratory = Laboratory.all_objects.get(
+            deleted_isnull=True,
+            pk=pk
+        )
         #
         all_users = Employee.all_objects.filter(deleted__isnull=True)
         selected_users = laboratory.employees.all()
         #
-        all_service_hours = LaboratoryServiceHours.all_objects.filter(deleted__isnull=True)
+        all_service_hours = LaboratoryServiceHours.all_objects.filter(
+            deleted__isnull=True
+        )
         selected_service_hours = laboratory.service_hours
         #
         all_inventories = Inventory.all_objects.filter(deleted__isnull=True)
@@ -182,9 +213,9 @@ def track_services(request, pk,
         'Diciembre'
     ]
 
-    #get only services of the current laboratory
-    all_services = ServiceRequest.objects.all()
-    laboratory = Laboratory.objects.get(pk=pk)
+    # get only services of the current laboratory
+    all_services = ServiceRequest.all_objects.all(deleted__isnull=True)
+    laboratory = Laboratory.all_objects.get(deleted__isnull=True, pk=pk)
     all_employes = laboratory.employees.all()
     service_request_list = []
     for service in all_services:
@@ -226,6 +257,6 @@ def track_services(request, pk,
     context = {
         'js_data': js_data,
         'actual_month': month_names[now.month - 1] + " " + str(now.year),
-        'laboratory' : laboratory
+        'laboratory': laboratory
     }
     return render(request, template, context)
