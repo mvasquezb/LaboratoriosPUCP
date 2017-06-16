8a2eb6f184e1ab88f86abae2066f32f1b7266a6f
Martín Vásquez <pedromvasquezb@gmail.com>
2017-06-16 17:45:54 -0500

    Fix middleware

--- laboratorios/internal/middleware.py
+++ laboratorios/internal/middleware.py
@@ -6,8 +6,9 @@ from django.utils.deprecation import MiddlewareMixin
 from django.conf import settings
 from django.core.urlresolvers import reverse_lazy
 
+
 class AuthRequiredMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        #if not request.user.is_authenticated() and request.path != '/internal/login':      # Descomentar
-            #return redirect('internal:login')                                              # Descomentar
+        if not request.user.is_authenticated() and request.path != '/internal/login':
+            return redirect('internal:login')
         return None
