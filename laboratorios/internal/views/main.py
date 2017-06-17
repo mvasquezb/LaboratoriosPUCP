c359b808f5ee5e5714523d5e009928c49d4003b9
Jose <jchudb93@gmail.com>
2017-06-16 19:13:29 -0500

    roles fixed

--- laboratorios/internal/views/main.py
+++ laboratorios/internal/views/main.py
@@ -9,16 +9,21 @@ __all__ = (
 
 
 def index(request, template = 'internal/index.html'):
-    employee = Employee.objects.get(pk=request.user.basicuser.id)
-    essay_method_fill_list = employee.assigned_essay_methods.all()
-    sample_list = []
-    for i in range(0,len(essay_method_fill_list)):
-        sample = Sample.all_objects.get(deleted__isnull=True,pk=essay_method_fill_list[i].essay.sample.id)
-        parameters_list = EssayMethodParameterFill.all_objects.filter(deleted__isnull=True,essay_method=essay_method_fill_list[i],value__isnull=True)
-        sample_list.append({'sample':sample,
+    if hasattr(request.user, 'basicuser'):
+        employee = Employee.objects.get(pk=request.user.basicuser.id)
+        essay_method_fill_list = employee.assigned_essay_methods.all()
+        sample_list = []
+        for i in range(0,len(essay_method_fill_list)):
+            sample = Sample.all_objects.get(deleted__isnull=True,pk=essay_method_fill_list[i].essay.sample.id)
+            parameters_list = EssayMethodParameterFill.all_objects.filter(deleted__isnull=True,essay_method=essay_method_fill_list[i],value__isnull=True)
+            sample_list.append({'sample':sample,
                             'filled':not parameters_list.exists()})
-    context = {
-        'methods_list':  essay_method_fill_list,
-        'samples_list':sample_list
+        context = {
+            'methods_list':  essay_method_fill_list,
+            'samples_list':sample_list
+        }
+    context = { 
+        'methods_list':  [],
+            'samples_list': []
     }
     return render(request,template,context)
