bc8debf4001b6a593c3b351d671287123bc89ca5
Martín Vásquez <pedromvasquezb@gmail.com>
2017-06-17 23:43:56 -0500

    Fix logic error. Some style changes in internal.index view

--- laboratorios/internal/views/main.py
+++ laboratorios/internal/views/main.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+# from django.contrib.auth.decorators import login_required
+# from django.utils.decorators import method_decorator
 from internal.models import *
 
 __all__ = (
@@ -8,22 +8,34 @@ __all__ = (
 )
 
 
-def index(request, template = 'internal/index.html'):
+def index(request, template='internal/index.html'):
     if hasattr(request.user, 'basicuser'):
-        employee = Employee.objects.get(pk=request.user.basicuser.id)
+        employee = Employee.all_objects.get(
+            deleted__isnull=True,
+            pk=request.user.basicuser.id
+        )
         essay_method_fill_list = employee.assigned_essay_methods.all()
         sample_list = []
-        for i in range(0,len(essay_method_fill_list)):
-            sample = Sample.all_objects.get(deleted__isnull=True,pk=essay_method_fill_list[i].essay.sample.id)
-            parameters_list = EssayMethodParameterFill.all_objects.filter(deleted__isnull=True,essay_method=essay_method_fill_list[i],value__isnull=True)
-            sample_list.append({'sample':sample,
-                            'filled':not parameters_list.exists()})
+        for essay_method_fill in essay_method_fill_list:
+            sample = Sample.all_objects.get(
+                deleted__isnull=True,
+                pk=essay_method_fill.essay.sample.id
+            )
+            parameters_list = EssayMethodParameterFill.all_objects.filter(
+                deleted__isnull=True,
+                essay_method=essay_method_fill.essay_method,
+                value__isnull=True
+            )
+            sample_list.append({
+                'sample': sample,
+                'filled': not parameters_list.exists()
+            })
         context = {
-            'methods_list':  essay_method_fill_list,
-            'samples_list':sample_list
+            'methods_list': essay_method_fill_list,
+            'samples_list': sample_list
         }
-    context = { 
-        'methods_list':  [],
-            'samples_list': []
+    context = {
+        'methods_list': [],
+        'samples_list': []
     }
-    return render(request,template,context)
+    return render(request, template, context)
