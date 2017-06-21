e757be098014dd09512cdbfe1f251ccbc9e712d2
Martín Vásquez <pedromvasquezb@gmail.com>
2017-06-21 17:00:37 -0500

    Restore middleware

--- laboratorios/internal/middleware.py
+++ laboratorios/internal/middleware.py
@@ -1,14 +1,9 @@
-from django.shortcuts import (
-    HttpResponseRedirect,
-    redirect
-)
+from django.shortcuts import redirect
 from django.utils.deprecation import MiddlewareMixin
-from django.conf import settings
-from django.core.urlresolvers import reverse_lazy
 
 
 class AuthRequiredMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        #if not request.user.is_authenticated() and request.path != '/internal/login':
-        #    return redirect('internal:login')
+        if not request.user.is_authenticated() and request.path != '/internal/login':
+            return redirect('internal:login')
         return None
