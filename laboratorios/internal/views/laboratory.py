817c9225714c3c395b771ff1ca697535e5b48b31
Martín Vásquez <pedromvasquezb@gmail.com>
2017-06-23 00:13:44 -0500

    Style changes

--- laboratorios/internal/views/laboratory.py
+++ laboratorios/internal/views/laboratory.py
@@ -13,6 +13,7 @@ import json as simplejson
 from django.http import JsonResponse
 from django.http import HttpResponse
 
+
 def index(request,
           template='internal/laboratory/index.html',
           extra_context=None):
@@ -24,19 +25,21 @@ def index(request,
         context.update(extra_context)
     return render(request, template, context)
 
+
 def inventory_modal(request):
     if request.method == 'GET' and request.is_ajax():
-        #print("LLEGA BIEN AL VIEW")
+        # print("LLEGA BIEN AL VIEW")
         dicc = dict(request.GET)
         inventory_pk = dicc['inventory_pk'][0]
-        #print(inventory_pk)
+        # print(inventory_pk)
         inventory = Inventory.all_objects.get(pk=inventory_pk)
-        article_inventory = ArticleInventory.all_objects.filter(inventory=inventory)
+        article_inventory = ArticleInventory.all_objects.filter(
+            inventory=inventory)
         matches_list = []
         for match in article_inventory:
             article_name = match.article.name
             article_quantity = match.article.quantity
-            matches_list.append((article_name,article_quantity))
+            matches_list.append((article_name, article_quantity))
         data = {
             'inventory_name': inventory.name,
             'inventory_location': inventory.location,
@@ -45,6 +48,7 @@ def inventory_modal(request):
         }
         return JsonResponse(data)
     return HttpResponse("GG")
+
 
 def services_index(request,
                    pk):
@@ -140,7 +144,7 @@ def create(request,
     form = LaboratoryForm(request.POST or None)
     if request.method == 'POST':
         if form.is_valid():
-            #here we add essay_methods to every employee of the new laboratory
+            # here we add essay_methods to every employee of the new laboratory
             for employee in form.cleaned_data['employees']:
                 for essay_method in form.cleaned_data['essay_methods']:
                     employee.essay_methods.add(essay_method)
@@ -159,7 +163,9 @@ def create(request,
 
             # return HttpResponse(form.errors)
     else:
-        #users = Employee.all_objects.filter(deleted__isnull=True,laboratories__isnull=True) #just active users
+        # users =
+        # Employee.all_objects.filter(deleted__isnull=True,laboratories__isnull=True)
+        # #just active users
         users = Employee.all_objects.annotate(
             labs=Count('laboratories')
         ).filter(
@@ -266,10 +272,16 @@ def show(request,
         selected_essaymethods = laboratory.essay_methods.all()
         #
         form = LaboratoryForm()
-        context = {'laboratory': laboratory, 'all_users': all_users,
-                   'selected_users': selected_users, 'all_inventories': all_inventories, 'selected_inventories': selected_inventories,
-                   'all_essaymethods': all_essaymethods, 'selected_essaymethods': selected_essaymethods,
-                   'form': form}
+        context = {
+            'laboratory': laboratory,
+            'all_users': all_users,
+            'selected_users': selected_users,
+            'all_inventories': all_inventories,
+            'selected_inventories': selected_inventories,
+            'all_essaymethods': all_essaymethods,
+            'selected_essaymethods': selected_essaymethods,
+            'form': form
+        }
         template = 'internal/laboratory/show.html'
         return render(request, template, context)
 
