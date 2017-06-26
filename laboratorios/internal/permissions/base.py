b00558b43b9f9cd51df1ef7c176ed6462bbf941b
Martín Vásquez <pedromvasquezb@gmail.com>
2017-06-25 21:52:17 -0500

    Add 'set_permission_check' util function

--- laboratorios/internal/permissions/base.py
+++ laboratorios/internal/permissions/base.py
@@ -1,4 +1,11 @@
+import functools
+
+
 def permission_check(user, permission):
     if not hasattr(user, 'basicuser'):
         return False
     return permission in user.basicuser.permissions()
+
+
+def set_permission_check(permission):
+    return functools.partial(permission_check, permission=permission)
