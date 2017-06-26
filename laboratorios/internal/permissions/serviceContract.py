832edbc8818625454bb5d5910e7806daf163a7f3
Martín Vásquez <pedromvasquezb@gmail.com>
2017-06-26 00:46:45 -0500

    Add index, show checks for 'ServiceContract'

--- laboratorios/internal/permissions/serviceContract.py
+++ laboratorios/internal/permissions/serviceContract.py
@@ -3,3 +3,5 @@ from . import set_permission_check
 
 edit_service_contract_check = set_permission_check('change_servicecontract')
 delete_service_contract_check = set_permission_check('delete_servicecontract')
+show_service_contract_check = set_permission_check('show_servicecontract')
+index_service_contract_check = set_permission_check('index_servicecontract')
--- laboratorios/internal/views/servicecontract.py
+++ laboratorios/internal/views/servicecontract.py
@@ -11,6 +11,7 @@ from django.contrib.auth.decorators import user_passes_test
 from internal.permissions.serviceContract import *
 
 
+@user_passes_test(index_service_contract_check, login_url='internal:index')
 def index(request,
           template='internal/servicecontract/index.html',
           extra_context=None):
@@ -23,6 +24,7 @@ def index(request,
     return render(request, template, context)
 
 
+@user_passes_test(show_service_contract_check, login_url='internal:index')
 def show(request,
          service_id,
          template='internal/servicecontract/show.html'):
