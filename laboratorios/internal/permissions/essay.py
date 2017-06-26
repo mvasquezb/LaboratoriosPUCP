3f5ca0bed166c67d961000875f79de127367bb75
Martín Vásquez <pedromvasquezb@gmail.com>
2017-06-26 00:41:03 -0500

    Add index, show checks for 'Essay'

--- laboratorios/internal/permissions/essay.py
+++ laboratorios/internal/permissions/essay.py
@@ -4,3 +4,5 @@ from . import set_permission_check
 create_essay_check = set_permission_check('add_essay')
 edit_essay_check = set_permission_check('change_essay')
 delete_essay_check = set_permission_check('delete_essay')
+index_essay_check = set_permission_check('index_essay')
+show_essay_check = set_permission_check('show_essay')
