edcbc230f8dba4e1f3842fa47c89998e8cee34ab
akawa <serdelrio94@gmail.com>
2017-06-28 05:42:36 -0500

    filtering process almost finished

--- laboratorios/internal/views/reports.py
+++ laboratorios/internal/views/reports.py
@@ -37,11 +37,12 @@ def report_parameters(request,
     
     # as we do not want to mantain later the logic bc of more fitlers,
     # lets use a dictionary
+    #squared = list(map(lambda x: x**2, items))
     data_list={
-        'client':[o.__str__() for o in Client.all_objects.filter(deleted__isnull=True).order_by('user','id')[::1]],
-        'sample_type':[o.name for o in SampleType.all_objects.filter(deleted__isnull=True).order_by('name','id')[::1]],
-        'laboratory':[o.name for o in Laboratory.all_objects.filter(deleted__isnull=True).order_by('name','id')[::1]],
-        'essay':[o.name for o in Essay.all_objects.filter(deleted__isnull=True).order_by('name','id')[::1]]
+        'client': list(map((lambda x: {'id':x.id,'str':x.user}),Client.all_objects.filter(deleted__isnull=True).order_by('user','id')[::1])),
+        'sample_type': list(map(lambda x: {'id':x.id,'str':x.name},SampleType.all_objects.filter(deleted__isnull=True).order_by('name','id')[::1])),
+        'laboratory':list(map(lambda x: {'id':x.id,'str':x.name},Laboratory.all_objects.filter(deleted__isnull=True).order_by('name','id')[::1])),
+        'essay':list(map(lambda x: {'id':x.id,'str':x.name},Essay.all_objects.filter(deleted__isnull=True).order_by('name','id')[::1]))
     }
 
     # Because we wont always check for all filters, we should have a
@@ -61,6 +62,12 @@ def report_parameters(request,
         'essay':'Ensayo',
     }
 
+    criteria_selection=[
+        'client',
+        'sample_type',
+        'laboratory',
+        'essay',
+    ]
     criteria_selection_string = ['Cliente','Tipo de Muestra','Laboratorio','Ensayo']
 
     # The JSONField wil pickup the following:
@@ -81,15 +88,58 @@ def report_parameters(request,
         print('hello?')
         js_data = json.loads(json_form['js_data'].value())
         if js_data is not None:
+            js_data.update({
+                            'filter':filter_selection,
+                            'criteria_list':criteria_selection,
+                            'data_list':dict(map(lambda x: (x[0],list(map(lambda y: y['id'], x[1] ))) , data_list.items())),
+                            'start_date':js_data['start_date'][:10],
+                            'end_date':js_data['end_date'][:10],
+                            })
             request.session['report_settings']=js_data
-            print(request.session['report_settings'])
             request.session.modified = True
             return redirect(reverse('internal:reports.results'))
         else:
             print("que fuentes tmr?")
     return render(request,template,context)
 
+def client_group(context,settings):
+    # Filas        
+    # Num servicios en rango de fechas
+    table_label=['Nombre de Cliente', 'Número de Servicios','Costo promedio por Servicio','Costo acumulado de Servicios','Duración promedio de servicios']
+    table_matrix=[]
+    # Etapa de filtracion de resultados
+    if (len(settings['filters_data'][0]) >0):
+        client_list=list(map(lambda x:Client.all_objects.filter(deleted__isnull=True,pk=x).first(),list(map(lambda x: settings['data_list']['client'][x],settings['filters_data'][0]))))
+    else:
+        client_list=Client.all_objects.filter(deleted__isnull=True)[::1]
+    service_matrix=list(map(lambda x:ServiceRequest.all_objects.filter(deleted__isnull=True,client=x)[::1],client_list))
 
+    if(len(settings['filters_data'][1]) >0):
+        sample_list=list(map(lambda x:SampleType.all_objects.filter(deleted__isnull=True,pk=x).first(),list(map(lambda x: settings['data_list']['sample_type'][x],settings['filters_data'][1]))))
+        service_matrix=list(map(lambda x: [y for y in x if len(Sample.all_objects.filter(deleted__isnull=True,request=y,sample_type__in=sample_list))>0],service_matrix))
+
+    if(len(settings['filters_data'][2])>0):
+        laboratory_list=list(map(lambda x:Laboratory.all_objects.filter(deleted__isnull=True,pk=x).first(),list(map(lambda x: settings['data_list']['laboratory'][x],settings['filters_data'][2]))))
+        service_matrix=list(map(lambda x: [y for y in x if y.supervisor.laboratories.all().first() in laboratory_list],service_matrix))
+    if(len(settings['filters_data'][3])>0):
+        essay_list=list(map(lambda x:Essay.all_objects.filter(deleted__isnull=True,pk=x).first(),list(map(lambda x: settings['data_list']['essay'][x],settings['filters_data'][3]))))
+        service_matrix=list(map(lambda x: [y for y in x if len(EssayFill.all_objects.filter(deleted__isnull=True,essay__in=essay_list,sample=y.sample))>0],service_matrix))
+
+    print(client_list)  
+    print(sample_list)
+    print(laboratory_list)
+    print(service_matrix)
+    return context
+
+def sample_group(context,settings):
+    return context
+
+def laboratory_group(context,settings):
+    # Filas         Num Empleados, Num Servicios dentro de fechas, Num 
+    return context
+
+def essay_group(context,settings):
+    return context
 
 def processing_parameters(
     request,
@@ -98,9 +148,22 @@ def processing_parameters(
     try:
         settings=request.session.get('report_settings',[])
         print(settings)
-        del request.session['report_settings']
     except KeyError:
         print('not there yet')
+        settings={}
     context={}
+    if (len(settings) == 0):
+        return render(request,template,context)
+    # we only recieve id as those are what we need to reconstruct our list
+    if len(settings['criteria_list'])==settings['criteria']:
+        return render(request,template,context)
+    mycase = {
+        'client': client_group, #do not use ()
+        'sample_type': sample_group, #do not use ()
+        'laboratory': laboratory_group, #do not use ()
+        'essay': essay_group,
+    }
 
-    return render(request,template,context)
+    return render(request,template,mycase[settings['criteria_list'][settings['criteria']]](context,settings))
+
+    
